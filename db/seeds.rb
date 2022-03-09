55956408c7c6e47bd90e49a9d4bdd1a17681a135
deana <dvandegriff1@gmail.com>
2022-03-09 12:01:47 -0600

    adding products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+product = Product.new(name: "MacBook_Air", price: 999, image_url: "https://unsplash.com/photos/mP7aPSUm7aE", description: "a thin lightweight laptop that is ultraportable")
+product.save
+
+product1 = Product.new(name: "Scissors", price: 4, image_url: "https://www.dreamstime.com/stock-photos-scissors-image18931433", description: "hand operating shearing tool")
+product1.save

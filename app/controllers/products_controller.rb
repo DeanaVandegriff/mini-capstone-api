2d6748a517591548b6520fb03bb3bab4a149ace3
deana <dvandegriff1@gmail.com>
2022-03-09 12:46:54 -0600

    adding routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,20 @@
 class ProductsController < ApplicationController
   def index
   end
+
+  def all_products
+    products = Product.all
+    render json: products.as_json
+  end
+
+  def first_product
+    product = Product.first
+    render json: product.as_json
+  end
+
+  def last_product
+    product = Product.last
+    render json: product.as_json
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  get 'products/index'
+  # get 'products/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/all_products", controller: "products", action: "all_products"
+
+  get "/first_product", controller: "products", action: "first_product"
+
+  get "/last_product", controller: "products", action: "last_product"
 end

db6b2f7c51f26aecab67add78a81210184ad9b23
deana <dvandegriff1@gmail.com>
2022-03-11 14:30:58 -0600

    adding more Restful conventions

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,8 @@ Rails.application.routes.draw do
   get "/products" => "products#index"
 
   get "/products/:id" => "products#show"
+
+  post "/products" => "products#create"
+
+  patch "/products/:id" => "products#update"
 end

88c6557c33745369207c207c51822ce3d0293640
deana <dvandegriff1@gmail.com>
2022-03-14 14:17:05 -0500

    adding model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,18 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    if price < 10
+      p "true"
+    else
+      p "false"
+    end
+  end
+
+  def tax
+    tax = price * 0.09
+    return tax
+  end
+
+  def total
+    total = price + tax
+  end
 end
--- frontend-mini-capstone.rb
+++ frontend-mini-capstone.rb
@@ -4,6 +4,8 @@ require "http"
 # products = response.parse(:json)
 # pp products
 
-response = HTTP.get("http://localhost:3000/products/2.json")
-products = response.parse(:json)
-pp products
+# response = HTTP.get("http://localhost:3000/products/6.json")
+# products = response.parse(:json)
+# pp products
+
+response = HTTP.post("http://localhost:3000/products", :params => { name: "pencil", price: 2, image_url: "image.jpg", description: "a wooden writing instrument" })
